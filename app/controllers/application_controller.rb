a359a08a7076610daf0f7dcd49c0aa143b9e1d76
RoUchiha <swaggsikh@gmail.com>
2021-11-12 01:02:17 -0600

    commit tree 1b44089ac01c338362f7e0a44a758440a5037ac4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,56 @@
 
-require_relative '../../config/environment'
+require_relative "../../config/environment"
 
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
+    set :method_override, true
   end
 
-  get '/' do
+  get "/" do
+    redirect to "/articles"
   end
+
+  get "/articles" do
+    @articles = Article.all 
+    erb :index
+  end
+
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
+  end
+
+  post "/articles" do
+    @new_article = Article.create(:title => params[:title], :content => params[:content])
+
+    redirect to "/articles/#{Article.last.id}"
+  end
+
+  get "/articles/:id" do
+      @article = Article.find(params[:id])
+      erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(:title => params[:title])
+    @article.update(:content => params[:content])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.delete
+    redicrect to "/articles"
+  end
+
 end
